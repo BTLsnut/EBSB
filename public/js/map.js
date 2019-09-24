ebeecc122293acda42439f2367f734e00fe81d64
dksshddl <dksshddl@naver.com>
2019-09-24 17:14:49 +0900

    줌 오류 변경

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,11 @@
-var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
+window.onload = function () {
+    var mapOptions = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10
+    };
+
+    const map_div = document.getElementById('map');
+
+    var map = new naver.maps.Map(map_div);
 };
 
-var map = new naver.maps.Map('map', mapOptions);
